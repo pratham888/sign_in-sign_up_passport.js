cbdb07ddd41f4ff0f2af881bb6ef4b4519d117fb
Pratham <prathamkumar7050@gmail.com>
2023-07-14 19:00:48 +0530

    add a controller and a router for home

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// use express router
+app.use('/',require('./routs'));
+
+
 
 app.listen(port, function(err){
     if(err){
